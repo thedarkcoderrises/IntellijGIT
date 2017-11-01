95d9c5cd8955e1bbb457b2908d5cb0f309caff0b
theDarkCoderRises <thedarkcoderrises@gmail.com>
2017-11-01 22:57:58 +0530

    json logging

--- src/main/java/com/maventest/App.java
+++ src/main/java/com/maventest/App.java
@@ -1,13 +1,32 @@
 package com.maventest;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+
+    private static final Logger logger = LogManager.getLogger();
+
+    public static void main(String[] args) {
+
+        App a = new App();
+        a.printJSONLg();
+
     }
+
+void printJSONLg(){
+    int val1 = 10, val2 = 11, val3 = 12;
+    logger.trace("val1={}, val2={}, val3={}", val1, val2, val3);
+    logger.debug("val1={}, val2={}, val3={}", val1, val2, val3);
+    logger.info("val1={}, val2={}, val3={}", val1, val2, val3);
+    logger.warn("val1={}, val2={}, val3={}", val1, val2, val3);
+    logger.error("val1={}, val2={}, val3={}", val1, val2, val3);
+    logger.fatal("val1={}, val2={}, val3={}", val1, val2, val3);
+}
+
 }
