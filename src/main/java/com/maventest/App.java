9a68c8993598502e53fd9d2c28659045e7ddc568
theDarkCoderRises <thedarkcoderrises@gmail.com>
2017-11-07 22:41:26 +0530

    intellij updt

--- src/main/java/com/maventest/App.java
+++ src/main/java/com/maventest/App.java
@@ -20,13 +20,31 @@ public class App
     }
 
 void printJSONLg(){
-    int val1 = 10, val2 = 11, val3 = 12;
-    logger.trace("val1={}, val2={}, val3={}", val1, val2, val3);
-    logger.debug("val1={}, val2={}, val3={}", val1, val2, val3);
-    logger.info("val1={}, val2={}, val3={}", val1, val2, val3);
-    logger.warn("val1={}, val2={}, val3={}", val1, val2, val3);
-    logger.error("val1={}, val2={}, val3={}", val1, val2, val3);
-    logger.fatal("val1={}, val2={}, val3={}", val1, val2, val3);
-}
+
+    for (int i=0;i<20;i++){
+        if(i/2==0){
+            logger.debug("This is i ="+i);
+        }else{
+            logger.info("This is i ="+i);
+//                if(i==12){
+
+                    try {
+                        throw new NullPointerException();
+                    } catch (Exception e) {
+                        logger.error(e,e);
+                    }
+//                }
+
+        }
+
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    }
 
 }
